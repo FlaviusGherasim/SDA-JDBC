6c0d29a396f8372c2db5f7cae5cef1a75ffd4380
Flavius Gherasim <flavius.gherasim1@gmail.com>
2021-05-12 18:02:07 +0300

    Redid Employee and Project repositories

--- src/main/java/com/sda/flaviusgherasim/hibernate/repository/EmployeeRepository.java
+++ src/main/java/com/sda/flaviusgherasim/hibernate/repository/EmployeeRepository.java
@@ -1,2 +1,53 @@
-package com.sda.flaviusgherasim.hibernate.repository;public class EmployeeRepository {
+package com.sda.flaviusgherasim.hibernate.repository;
+
+import com.sda.flaviusgherasim.hibernate.model.Department;
+import com.sda.flaviusgherasim.hibernate.model.Employee;
+import com.sda.flaviusgherasim.hibernate.model.Project;
+import com.sda.flaviusgherasim.hibernate.utils.SessionManager;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class EmployeeRepository {
+
+
+    public Employee findById(Integer id)
+    {
+        Session session = SessionManager.getSessionFactory().openSession();
+
+        //The find method returns the object with the provided id
+        Employee employee = session.find(Employee.class, id);
+
+        session.close();
+
+        return employee;
+    }
+
+    public void save(Employee employee)
+
+    {
+        Session session= SessionManager.getSessionFactory().openSession();
+        Transaction transaction= session.beginTransaction();
+
+        session.save(employee);
+        transaction.commit();
+        session.close();
+    }
+
+    public void delete(Employee employee)
+    {
+        Session session= SessionManager.getSessionFactory().openSession();
+        Transaction transaction= session.beginTransaction();
+        session.delete(employee);
+        transaction.commit();
+        session.close();
+    }
+
+    public void update(Employee employee)
+    {
+        Session session= SessionManager.getSessionFactory().openSession();
+        Transaction transaction= session.beginTransaction();
+        session.update(employee);
+        transaction.commit();
+        session.close();
+    }
 }
--- src/main/java/com/sda/flaviusgherasim/hibernate/repository/ProjectRepository.java
+++ src/main/java/com/sda/flaviusgherasim/hibernate/repository/ProjectRepository.java
@@ -1,2 +1,53 @@
-package com.sda.flaviusgherasim.hibernate.repository;public class ProjectRepository {
+package com.sda.flaviusgherasim.hibernate.repository;
+
+import com.sda.flaviusgherasim.hibernate.model.Employee;
+import com.sda.flaviusgherasim.hibernate.model.Project;
+import com.sda.flaviusgherasim.hibernate.utils.SessionManager;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class ProjectRepository {
+
+
+    public Project findById(Integer id)
+    {
+        Session session = SessionManager.getSessionFactory().openSession();
+
+        //The find method returns the object with the provided id
+        Project project = session.find(Project.class, id);
+
+        session.close();
+
+        return project;
+    }
+
+    public void save(String projectDescription)
+
+    {
+        Session session= SessionManager.getSessionFactory().openSession();
+        Transaction transaction= session.beginTransaction();
+        Project project= new Project();
+        project.setName(projectDescription);
+        session.save(project);
+        transaction.commit();
+        session.close();
+    }
+
+    public void delete(Project project)
+    {
+        Session session= SessionManager.getSessionFactory().openSession();
+        Transaction transaction= session.beginTransaction();
+        session.delete(project);
+        transaction.commit();
+        session.close();
+    }
+
+    public void update(Project project)
+    {
+        Session session= SessionManager.getSessionFactory().openSession();
+        Transaction transaction= session.beginTransaction();
+        session.update(project);
+        transaction.commit();
+        session.close();
+    }
 }
